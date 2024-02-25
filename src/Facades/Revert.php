1c814764d367e611e9fbe9d179928b3e2cf1f485
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2024-02-26 06:40:01 +0800

    chore: add facade dochint

--- src/Facades/Revert.php
+++ src/Facades/Revert.php
@@ -5,9 +5,21 @@ declare(strict_types=1);
 namespace Storipress\Revert\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Storipress\Revert\Requests\Contact;
+use Storipress\Revert\Requests\Deal;
+use Storipress\Revert\Requests\Property;
 
 /**
  * @method static \Storipress\Revert\Revert instance()
+ * @method static string getToken()
+ * @method static \Storipress\Revert\Revert setToken()
+ * @method static string getCustomerId()
+ * @method static \Storipress\Revert\Revert setCustomerId()
+ * @method static string getUserAgent()
+ * @method static \Storipress\Revert\Revert setUserAgent()
+ * @method static Contact contact()
+ * @method static Deal deal()
+ * @method static Property property()
  */
 class Revert extends Facade
 {
