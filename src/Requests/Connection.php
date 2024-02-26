21decfc6c057bd33e291e977bad71200c00833ff
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2024-02-26 08:48:03 +0800

    chore: lint

--- src/Requests/Connection.php
+++ src/Requests/Connection.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Storipress\Revert\Requests;
 
-use stdClass;
-use Storipress\Revert\Objects\Crm\Deal as DealObject;
-use Storipress\Revert\Objects\CursorPagination;
-
 class Connection extends Request
 {
     /**
